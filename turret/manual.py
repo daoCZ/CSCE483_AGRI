a62fe18b0eefb6c438509d8732a0ded4c0dfecdc
daoCZ <87791158+daoCZ@users.noreply.github.com>
2023-11-26 23:39:07 -0600

    Add files via upload

--- turret/manual.py
+++ turret/manual.py
@@ -13,16 +13,23 @@ GPIO.setup(6, GPIO.OUT)
 GPIO.output(6, True)
 
 # Declare an named instance of class pass a name and motor type
-mymotortestOne = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
-mymotortestTwo = RpiMotorLib.BYJMotor("MyMotorTwo", "28BYJ")
+mymotortestYaw = RpiMotorLib.BYJMotor("MyMotorOne", "28BYJ")
+mymotortestPitch = RpiMotorLib.BYJMotor("MyMotorTwo", "28BYJ")
 
 # call the function , pass the parameters
 # SEQUENTIAL: mymotortest.motor_run(GpioPins , .001, 512, False, True, "full", .05)
 
+yawNEG = True
+yawSteps = 256
+
+pitchPOS = True
+pitchSteps = 256
+
+
 # CONCURRENT
 with concurrent.futures.ThreadPoolExecutor() as executor:
-        f1 = executor.submit(mymotortestOne.motor_run, GPIOyaw,.001 , 512, False, False,"full", .05)
-        f2 = executor.submit(mymotortestTwo.motor_run, GPIOpitch,.001 , 512, False, False,"full", .05)
+        f1 = executor.submit(mymotortestYaw.motor_run, GPIOyaw, .001 , yawSteps, yawNEG, False, "full", .05)
+        f2 = executor.submit(mymotortestPitch.motor_run, GPIOpitch, .001 , pitchSteps, pitchPOS, False, "full", .05)
 
 
 GPIO.output(6, False)
